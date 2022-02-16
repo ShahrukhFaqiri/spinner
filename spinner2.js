97bd4c0ab96b399e5288433739767b9436d37a1c
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-16 01:17:18 +0000

    syntax

--- spinner2.js
+++ spinner2.js
@@ -1,11 +1,9 @@
 //Refactored Spinner
-
-  let spinner = '|/-\\|/-\\|' + '\n';
-  let time = 0;
-  for (const n of spinner) {
-    setTimeout(() => {
-      process.stdout.write(`\r${n}`)
-    }, time);
-    time += 200;
-  }
-
+let spinner = '|/-\\|/-\\|' + '\n';
+let time = 0;
+for (const n of spinner) {
+  setTimeout(() => {
+    process.stdout.write(`\r${n}`);
+  }, time);
+  time += 200;
+}
